7ddf0f0c17139f45a76da3f40b2a9977272f8d76
samindriano <samuelindriano@gmail.com>
2026-02-20 00:10:37 +0700

    init spring boot

---
+++ src/main/java/id/ac/ui/cs/advprog/voucher/JsonVoucherServiceApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.voucher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonVoucherServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonVoucherServiceApplication.class, args);
+	}
+
+}
