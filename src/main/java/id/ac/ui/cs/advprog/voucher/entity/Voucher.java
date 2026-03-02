85707461bea0e6d2f2e7b8ab1297b745c5b8cc89
samindriano <samuelindriano@gmail.com>
2026-03-02 22:23:13 +0700

    minimizing getter and setter in Voucher using lombok

--- src/main/java/id/ac/ui/cs/advprog/voucher/entity/Voucher.java
+++ src/main/java/id/ac/ui/cs/advprog/voucher/entity/Voucher.java
@@ -4,7 +4,11 @@ import id.ac.ui.cs.advprog.voucher.exception.InvalidVoucherStateException;
 import id.ac.ui.cs.advprog.voucher.exception.VoucherQuotaExhaustedException;
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@NoArgsConstructor
 @Entity
 @Table(name = "vouchers")
 public class Voucher {
@@ -40,10 +44,6 @@ public class Voucher {
 
     @Column(nullable = false)
     private LocalDateTime createdAt = LocalDateTime.now();
-
-    public Voucher() {
-
-    }
 
     public Voucher(
             String code,
@@ -84,81 +84,5 @@ public class Voucher {
         if (this.quotaRemaining <= 0) {
             throw new VoucherQuotaExhaustedException();
         }
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public LocalDateTime getValidFrom() {
-        return validFrom;
-    }
-
-    public LocalDateTime getValidUntil() {
-        return validUntil;
-    }
-
-    public Integer getQuotaTotal() {
-        return quotaTotal;
-    }
-
-    public Integer getQuotaRemaining() {
-        return quotaRemaining;
-    }
-
-    public String getTerms() {
-        return terms;
-    }
-
-    public Integer getDiscountPercent() {
-        return discountPercent;
-    }
-
-    public Boolean getActive() {
-        return active;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public void setValidFrom(LocalDateTime validFrom) {
-        this.validFrom = validFrom;
-    }
-
-    public void setValidUntil(LocalDateTime validUntil) {
-        this.validUntil = validUntil;
-    }
-
-    public void setQuotaTotal(Integer quotaTotal) {
-        this.quotaTotal = quotaTotal;
-    }
-
-    public void setTerms(String terms) {
-        this.terms = terms;
-    }
-
-    public void setDiscountPercent(Integer discountPercent) {
-        this.discountPercent = discountPercent;
-    }
-
-    public void setActive(Boolean active) {
-        this.active = active;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
     }
 }
